3a024e63d303ae3dc8e37d270a2c46df1ca5a94b
mkristian <m.kristian@web.de>
2009-10-01 13:17:11 +0530

    tried to get the options right - but seems not to work

--- generators/datamapper_model/templates/migration.rb
+++ generators/datamapper_model/templates/migration.rb
@@ -1,12 +1,13 @@
 migration <%= Time.now.utc.strftime("%Y%m%d%H%M%S") %>, :<%= migration_name.underscore %> do
   up do
     create_table :<%= table_name %> do
+      column :id, Integer, :serial => true
 <% Array(attributes).each do |attribute| -%>
-      column :<%= attribute.name if attribute %>, <%= attribute.type.to_s.capitalize %>, :nullable? => false
+      column :<%= attribute.name if attribute %>, <%= attribute.type.to_s.capitalize %>, :nullable => false<% if attribute.type == :string %>, :length => 255<% end %>
 <% end -%>
 <% unless options[:skip_timestamps] -%>
-      column :created_at, DateTime, :nullable? => false
-      column :updated_at, DateTime, :nullable? => false
+      column :created_at, DateTime, :nullable => false
+      column :updated_at, DateTime, :nullable => false
 <% end -%>
     end
   end
