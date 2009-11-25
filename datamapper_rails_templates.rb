0c3625bf90f5149b540e4ba1c815e66c2267b3ae
mkristian <m.kristian@web.de>
2009-11-26 00:26:19 +0530

    removed dep to ixtlan

--- datamapper_rails_templates.rb
+++ datamapper_rails_templates.rb
@@ -57,7 +57,7 @@ initializer 'datamapper_store.rb', <<-CODE
 # init a session store which uses a memory cache and a persistent store
 # cleanup can be a problem. jruby uses soft-references for the cache so
 # memory cleanup with jruby is not a problem.
-require 'ixtlan/session'
+require 'datamapper4rails/datamapper_store'
 ActionController::Base.session_store = :datamapper_store
 ActionController::Base.session = {
   :cache       => true,
--- lib/datamapper4rails/version.rb
+++ lib/datamapper4rails/version.rb
@@ -1,3 +1,3 @@
 class Datamapper4rails
-  VERSION = '0.4.0'.freeze
+  VERSION = '0.3.2'.freeze
 end
