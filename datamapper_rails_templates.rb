38dc646fa5e093f55a667b789e55fd640c718bb4
mkristian <m.kristian@web.de>
2009-11-28 11:46:18 +0530

    fixed issue with datamapper edge

--- datamapper_rails_templates.rb
+++ datamapper_rails_templates.rb
@@ -1,8 +1,8 @@
 # inspired by http://www.rowtheboat.com/archives/32
 ###################################################
 
-# get all datamapper related gems
-gem 'addressable', :lib => 'addressable/uri'
+# this pulls in rails_datamapper and rack_datamapper
+gem 'datamapper4rails'
 
 # assume sqlite3 to be database
 gem 'do_sqlite3'
@@ -12,13 +12,14 @@ gem 'dm-validations'
 gem 'dm-timestamps'
 gem 'dm-migrations'
 gem 'dm-serializer' # to allow xml interface to work
+gem 'dm-core'
+
+# get all datamapper related gems
+gem 'addressable', :lib => 'addressable/uri'
 
 # assume you prefer rspec over unit tests
 gem 'rspec', :lib => false
 gem 'rspec-rails', :lib => false
-
-# this pulls in rails_datamapper and rack_datamapper
-gem 'datamapper4rails'
 
 # install all gems
 rake 'gems:install'
@@ -81,7 +82,7 @@ file 'prepare_jruby.sh', <<-CODE
 
 echo
 echo "shall freeze rails and fix a bug which prevents rails to use certain"
-echo "java gems like the dataobjects drivers !!"
+echo "java gems like the dataobjects drivers (do_sqlite3, etc) !!"
 echo
 
 mvn --version
@@ -91,12 +92,12 @@ if [ $? -ne 0 ] ; then
         exit -1
 fi
 
-mvn de.saumya.mojo:rails-maven-plugin:rails-freeze-gems de.saumya.mojo:rails-maven-plugin:gems-install
+mvn de.saumya.mojo:rails-maven-plugin:gems-install de.saumya.mojo:rails-maven-plugin:rails-freeze-gems de.saumya.mojo:rails-maven-plugin:gems-install -Djruby.fork=false
 
 echo
 echo "you can run rails with (no need to install jruby !!)"
 echo
-echo "\tmvn de.saumya.mojo:rails-maven-plugin:server"
+echo "\tmvn de.saumya.mojo:rails-maven-plugin:server -Djruby.fork=false"
 echo
 echo "more info on"
 echo "\thttp://github.org/mkristian/rails-maven-plugin"
@@ -111,4 +112,10 @@ logger.info
 logger.info "info mavenized rails application"
 logger.info "\thttp://github.org/mkristian/rails-maven-plugin"
 logger.info 
+logger.info "if you want to run jruby please run again after uninstalling"
+logger.info "the native extension of do_sqlite3"
+logger.info "\truby -S gem uninstall do_sqlite3"
+logger.info "\tjruby -S rake gems:install"
+logger.info "rake gems:unpack does NOT work with jruby due to a bug in rail <=2.3.4"
+logger.info "you can try the prepare-jruby.sh script and see if this works for you"
 logger.info 
